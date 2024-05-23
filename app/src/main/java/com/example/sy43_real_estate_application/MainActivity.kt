4ee538b52303b2ba86953f1858f82a914370d481
Gabriel Fleuret <gabfleuret@gmail.com>
2024-05-23 09:43:08 +0200

    project creation

--- app/src/main/java/com/example/sy43_real_estate_application/MainActivity.kt
+++ app/src/main/java/com/example/sy43_real_estate_application/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sy43_real_estate_application.ui.theme.SY43_real_estate_applicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,13 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Surface {
+                        Text(text = "Hello, World!")
+                    }
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
+                        Greeting(name = "Android")
+
                 }
             }
         }
