a30edc1de5cfc3dff7dc1765389971cb092ae41e
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2024-06-02 16:31:41 +0200

    = resolving bug with the api

--- app/src/main/java/com/example/sy43_real_estate_application/ui/theme/PhotoViewModel.kt
+++ app/src/main/java/com/example/sy43_real_estate_application/ui/theme/PhotoViewModel.kt
@@ -2,13 +2,13 @@ package com.example.sy43_real_estate_application.ui.theme
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.sy43_real_estate_application.network.PhotosApi
+import com.example.sy43_real_estate_application.network.RealEstateApi
 import kotlinx.coroutines.launch
 
 class PhotoViewModel : ViewModel() {
     fun getPhotos() {
         viewModelScope.launch {
-            val listResult = PhotosApi.retrofitService.getPhotos()
+            val listResult = RealEstateApi.retrofitService.getProperties()
             println(listResult)
 
         }
